0b4a270c5947b4711660ebc4b0c9f03f94a5ac22
Oleksandr Korotetskyi <oleksandr.korotetskyi@student.tuke.sk>
2019-10-28 11:06:13 +0100

    2

--- src/main/java/sk/tuke/kpi/oop/game/Computer.java
+++ src/main/java/sk/tuke/kpi/oop/game/Computer.java
@@ -5,11 +5,11 @@ import sk.tuke.kpi.gamelib.graphics.Animation;
 
 public class Computer extends AbstractActor {
 
-    private Animation normalAnimation;
+    private Animation animation;
 
     public Computer() {
-        this.normalAnimation =  new Animation("sprites/computer.png", 80, 48, 0.2f, Animation.PlayMode.LOOP_PINGPONG);
-        setAnimation(normalAnimation);
+        this.animation = new Animation("sprites/computer.png", 80, 48, 0.2f, Animation.PlayMode.LOOP_PINGPONG);
+        setAnimation(animation);
     }
 
     public int add(int a, int b) {
