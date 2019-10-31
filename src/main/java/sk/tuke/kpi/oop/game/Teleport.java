65abffbac7b6a656faeaa6f680fa71249c359dd1
Oleksandr Korotetskyi <oleksandr.korotetskyi@student.tuke.sk>
2019-10-31 21:07:11 +0100

    <Teleport> second  try '19:51:00'

--- src/main/java/sk/tuke/kpi/oop/game/Teleport.java
+++ src/main/java/sk/tuke/kpi/oop/game/Teleport.java
@@ -9,6 +9,94 @@ import sk.tuke.kpi.gamelib.framework.Player;
 import sk.tuke.kpi.gamelib.framework.actions.Loop;
 import sk.tuke.kpi.gamelib.graphics.Animation;
 
+
+public class Teleport extends AbstractActor {
+
+    private Teleport destinationTeleport;
+    private boolean teleportAccess;
+    private boolean wasOutside;
+
+    public Teleport() {
+        this(null);
+    }
+
+    public Teleport(Teleport teleport) {
+        Animation animation = new Animation("sprites/lift.png", 48, 48);
+        setAnimation(animation);
+        destinationTeleport = null;
+        setDestination(teleport);
+        teleportAccess = true;
+        wasOutside = false;
+    }
+
+    public Teleport getDestination() {
+        return destinationTeleport;
+    }
+
+    public void setDestination(Teleport teleport) {
+        if(this.equals(teleport) && destinationTeleport == null) return;
+        else if(this.equals(teleport) && destinationTeleport != null) return;
+        else destinationTeleport = teleport;
+    }
+
+    public void teleportPlayer(Player player) {
+        if(player != null) player.setPosition(getPosX() + getWidth() / 2 - player.getWidth()/2, getPosY() + getHeight() / 2 - player.getHeight()/2);
+        if(player != null && destinationTeleport != null && teleportAccess) {
+            player.setPosition(destinationTeleport.getPosX() + destinationTeleport.getWidth() / 2 - player.getWidth()/2, destinationTeleport.getPosY() + destinationTeleport.getHeight() / 2 - player.getHeight()/2);
+
+            destinationTeleport.wasOutside = false;
+            destinationTeleport.teleportAccess = false;
+            destinationTeleport.teleportPlayer(player);
+            this.wasOutside = false;
+            this.teleportAccess = false;
+
+        }
+    }
+
+    private Player getPlayer() {
+        return (Player) getScene().getFirstActorByName("Player");
+    }
+
+    public boolean isOutside() {
+        Actor player = getPlayer();
+        if(player != null) return !this.intersects(player);
+        return false;
+    }
+
+    private void wasPlayerOutside() {
+        Player player = getPlayer();
+        if(player != null) {
+            int qY = player.getPosY() + player.getHeight() / 2;
+            int qX = player.getPosX() + player.getWidth() / 2;
+            if ((qY < this.getPosY() || qY > (this.getPosY() + this.getHeight())) || (qX < this.getPosX() && qX > (this.getPosX() + this.getWidth()))) {
+                wasOutside = true;
+            }
+        }
+    }
+
+    private void isPlayerInside(){
+        Player player = getPlayer();
+        if(player != null && !teleportAccess && wasOutside) {
+            int qY = player.getPosY() + player.getHeight() / 2;
+            int qX = player.getPosX() + player.getWidth() / 2;
+            if ((qY >= this.getPosY() && qY <= (this.getPosY() + this.getHeight())) && (qX >= this.getPosX() && qX <= (this.getPosX() + this.getWidth()))) {
+                teleportAccess = true;
+                teleportPlayer(player);
+            }
+        }
+    }
+
+    @Override
+    public void addedToScene(@NotNull Scene scene) {
+        super.addedToScene(scene);
+        this.wasOutside = isOutside();
+        new Loop<Teleport>(new Invoke<>(this::wasPlayerOutside)).scheduleFor(this);
+        new Loop<Teleport>(new Invoke<>(this::isPlayerInside)).scheduleFor(this);
+    }
+}
+
+/*
+
 public class Teleport extends AbstractActor {
 
     private Teleport destinationTeleport;
@@ -40,7 +128,7 @@ public class Teleport extends AbstractActor {
 
     public void teleportPlayer(Player player) {
         if(player != null && destinationTeleport != null && teleportAccess) {
-            player.setPosition(destinationTeleport.getPosX() + destinationTeleport.getWidth() / 2, destinationTeleport.getPosY() + destinationTeleport.getHeight() / 2);
+            player.setPosition(destinationTeleport.getPosX() + destinationTeleport.getWidth() / 2 - player.getWidth()/2, destinationTeleport.getPosY() + destinationTeleport.getHeight() / 2 - player.getHeight()/2);
 
             destinationTeleport.wasOutside = false;
             destinationTeleport.teleportAccess = false;
@@ -91,3 +179,4 @@ public class Teleport extends AbstractActor {
         new Loop<Teleport>(new Invoke<>(this::isPlayerInside)).scheduleFor(this);
     }
 }
+ */
