71a29311ec930c85f9d90eb570bad3a04485d80e
Oleksandr Korotetskyi <oleksandr.korotetskyi@student.tuke.sk>
2019-11-03 12:37:41 +0100

    reactor changes '12:30:47'

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -1,118 +1,49 @@
 package sk.tuke.kpi.oop.game;
 
-import org.jetbrains.annotations.NotNull;
+import sk.tuke.kpi.gamelib.Actor;
 import sk.tuke.kpi.gamelib.Scene;
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
 import sk.tuke.kpi.gamelib.graphics.Animation;
 import sk.tuke.kpi.oop.game.actions.PerpetualReactorHeating;
+import sk.tuke.kpi.oop.game.tools.Usable;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class Reactor extends AbstractActor implements Switchable, Repairable {
+public class Reactor extends AbstractActor implements Switchable, EnergyConsumer, Repairable {
 
-    private static final byte REACTOR_OFF = 0;
-    private static final byte REACTOR_BROKEN = 1;
-    private static final byte REACTOR_EXTINGUISHED = 2;
-
-    private final Animation A_reactorOn = new Animation("sprites/reactor_on.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
-    private final Animation A_reactorOff = new Animation("sprites/reactor.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
-    private final Animation A_reactorHot = new Animation("sprites/reactor_hot.png", 80, 80, 0.06f, Animation.PlayMode.LOOP_PINGPONG);
-    private final Animation A_reactorBroken = new Animation("sprites/reactor_broken.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
-    private final Animation A_reactorExtinguished = new Animation("sprites/reactor_extinguished.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
-
-    private byte state;
-    private int temperature;
-    private int damage;
-    private boolean isOn;
-
+    private int                 temperature;
+    private int                 damage;
+    private boolean             isAlive;
     private Set<EnergyConsumer> devices;
+    private Animation           animation;
+    private Animation           reactorOff;
+    private Animation           reactor_extinguished;
+    private Animation           normalAnimation;
+    private Animation           hotAnimation;
+    private Animation           brokenAnimation;
 
     public Reactor() {
-        state = REACTOR_OFF;
         temperature = 0;
-        isOn = false;
         damage = 0;
-        setAnimation(A_reactorOff);
+        isAlive = false;
         devices = new HashSet<>();
-    }
+        normalAnimation = new Animation("sprites/reactor_on.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
+        hotAnimation = new Animation("sprites/reactor_hot.png", 80, 80, 0.05f, Animation.PlayMode.LOOP_PINGPONG);
+        brokenAnimation = new Animation("sprites/reactor_broken.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
+        reactor_extinguished = new Animation("sprites/reactor_extinguished.png");
+        reactorOff = new Animation("sprites/reactor.png");
 
-    public int getState() {
-        return state;
-    }
-
-    public int getDamage() {
-        return damage;
-    }
-
-    public int getTemperature() {
-        return temperature;
-    }
-
-    public void setDamage(int damage) {
-        this.damage = damage;
-    }
-
-    public void increaseTemperature(int increment) {
-        if (increment <= 0 || !isOn) return;
-
-        float coefficient = ((damage >= 33 && damage <= 66) ? 1.5f : ((damage > 66) ? 2f : 1f));
-
-        double newIncrement = increment * coefficient;
-        newIncrement = Math.ceil(newIncrement);
-
-        double newDamage = ((float) (temperature + newIncrement) - 2000f) / 40f;
-        newDamage = (newDamage > 0) ? (int) (newDamage - newDamage % 1) : damage;
-
-        int newTemperature = (int) (temperature + newIncrement);
-
-        damage = (newDamage > 100 || newTemperature > 6000) ? 100 : (int) newDamage;
-        temperature = newTemperature;
-
-        updateDevicesPowering();
-        updateAnimation();
-    }
-
-    public void decreaseTemperature(int decrement) {
-        if (decrement <= 0 || !isOn()) {
-            return;
-        }
-
-        if (getTemperature() != 0) {
-
-            int realDecrement = decrement;
-            if (getDamage() >= 50) realDecrement *= 0.5;
-            else if (damage == 100) realDecrement = 0;
-
-            temperature -= realDecrement;
-            if (temperature < 0) temperature = 0;
-
-            updateAnimation();
-        }
-    }
-
-    private void updateAnimation() {
-        if (!isOn) return;
-        if (temperature <= 4000) {
-            setAnimation(A_reactorOn);
-        } else if (temperature < 6000) {
-            setAnimation(A_reactorHot);
-        } else {
-            setAnimation(A_reactorBroken);
-            state = REACTOR_BROKEN;
-            isOn = false;
-            if(devices != null) {
-                for (EnergyConsumer e : devices) {
-                    e.setPowered(false);
-                }
-            }
-        }
+        animation = reactorOff;
+        setAnimation(animation);
     }
 
     @Override
     public boolean repair() {
-        if (damage > 0 && damage < 100) {
-            damage = ((damage - 50) < 0) ? 0 : damage - 50;
+        if (damage > 0) {
+            damage -= 50;
+            if (damage < 0)
+                damage = 0;
             temperature = (int) Math.ceil((damage / 0.025f) + 2000);
             updateAnimation();
             return true;
@@ -120,74 +51,167 @@ public class Reactor extends AbstractActor implements Switchable, Repairable {
         return false;
     }
 
+    @Override
+    public void setPowered(boolean isPowered) {
+        if (isPowered)
+            turnOn();
+        else
+            turnOff();
+    }
+
+    @Override
+    public void addedToScene(Scene scene) {
+        super.addedToScene(scene);
+        //new PerpetualReactorHeating(1).scheduleFor(this);
+        scene.scheduleAction(new PerpetualReactorHeating(1), this);
+    }
+
     public boolean extinguish() {
-        if (state == REACTOR_BROKEN) {
-            decreaseTemperature(4000);
-            if (isOn) isOn = false;
-            state = REACTOR_EXTINGUISHED;
-            setAnimation(A_reactorExtinguished);
+        if (animation == brokenAnimation) {
+            temperature -= 4000;
+            animation = reactor_extinguished;
+            setAnimation(animation);
             return true;
         }
         return false;
     }
 
-    @Override
-    public boolean isOn() {
-        return this.isOn;
+    public void addDevice(EnergyConsumer energyConsumer) {
+        devices.add(energyConsumer);
+        if (isOn() && isAlive)
+            energyConsumer.setPowered(true);
+        else
+            energyConsumer.setPowered(false);
     }
 
-    @Override
-    public void turnOff() {
-        if (state == REACTOR_BROKEN) return;
-        isOn = false;
-        if (devices != null) {
-            for (EnergyConsumer e : devices){
-                e.setPowered(false);
-            }
+    public void removeDevice(EnergyConsumer energyConsumer) {
+        if (energyConsumer != null) {
+            energyConsumer.setPowered(false);
+            devices.remove(energyConsumer);
         }
-        setAnimation(A_reactorOff);
     }
 
-    @Override
+    public boolean isOn() {
+        return isAlive;
+    }
+
     public void turnOn() {
-        isOn = true;
+        isAlive = true;
         if (devices != null) {
-            for (EnergyConsumer e : devices){
-                if (isOn()) {
-                    e.setPowered(true);
-                }
+            for (EnergyConsumer energyConsumer : devices){
+                if (isOn())
+                    energyConsumer.setPowered(true);
             }
         }
         updateAnimation();
     }
 
-    public void addDevice(EnergyConsumer energyConsumer) {
-        if (isOn) {
-            if (energyConsumer != null) devices.add(energyConsumer);
-            if (isOn && state != REACTOR_BROKEN && energyConsumer != null) energyConsumer.setPowered(true);
-        }
-    }
-
-    public void removeDevice(EnergyConsumer energyConsumer) {
-        if (energyConsumer != null) {
-            devices.remove(energyConsumer);
-            energyConsumer.setPowered(false);
-        }
-    }
-
-    @Override
-    public void addedToScene(@NotNull Scene scene) {
-        super.addedToScene(scene);
-        new PerpetualReactorHeating(1).scheduleFor(this);
-    }
-
-    public void updateDevicesPowering(){
-        if(temperature >= 6000 && devices != null) {
-            for (EnergyConsumer e : devices) {
-                e.setPowered(false);
+    public void turnOff() {
+        isAlive = false;
+        if (devices != null) {
+            for (EnergyConsumer energyConsumer : devices){
+                energyConsumer.setPowered(false);
             }
         }
+        updateAnimation();
     }
+
+    public void increaseTemperature(int increment) {
+        if (increment < 0 || !isAlive)
+            return;
+        //if (damage < 100) {
+        if (damage >= 33 && damage <= 66)
+            temperature += Math.round(increment * 1.5f);
+        else if (damage > 66)
+            temperature += increment * 2;
+        else
+            temperature += increment;
+        if (temperature >= 2000)
+            damage = (int) Math.floor((temperature - 2000) * 0.025f);
+        if (temperature >= 6000) {
+            damage = 100;
+            for (EnergyConsumer energyConsumer : devices){
+                energyConsumer.setPowered(false);
+            }
+        }
+
+        updateAnimation();
+        //}
+    }
+
+    public boolean repair(Usable<Actor> hammer) {
+        if (damage > 0) {
+            hammer.useWith(this);
+            damage -= 50;
+            if (damage < 0)
+                damage = 0;
+            temperature -= 2000;
+
+            updateAnimation();
+
+            return true;
+        }
+        return false;
+    }
+
+    public void decreaseTemperature(int decrement) {
+        if (decrement < 0 || temperature < 0 || !isAlive)
+            return;
+        if (damage < 100) {
+            if (damage >= 50)
+                temperature -= Math.round(decrement * 0.5f);
+            else
+                temperature -= decrement;
+
+            updateAnimation();
+        }
+    }
+
+    private void updateAnimation() {
+        if (isAlive) {
+            if (damage >= 100) {
+                animation = brokenAnimation;
+                turnOff();
+            }
+            if (temperature >= 4000 && temperature < 6000)
+                animation = hotAnimation;
+            if (temperature < 4000)
+                animation = normalAnimation;
+
+            //changeFrameDuration();
+        }
+        else if (damage < 100)
+            animation = reactorOff;
+
+        setAnimation(animation);
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    /*public void changeFrameDuration() {
+        if (temperature >= 0 && temperature <= 1000)
+            animation.setFrameDuration(0.1f);
+        else if (temperature > 1000 && temperature <= 2000)
+            animation.setFrameDuration(0.09f);
+        else if (temperature > 2000 && temperature <= 3000)
+            animation.setFrameDuration(0.07f);
+        else if (temperature > 3000 && temperature <= 4000)
+            animation.setFrameDuration(0.06f);
+        else if (temperature > 4000 && temperature <= 5000)
+            animation.setFrameDuration(0.05f);
+        else if (temperature > 5000 && temperature < 6000)
+            animation.setFrameDuration(0.03f);
+    }
+
+     */
 }
-
-
